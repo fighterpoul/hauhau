7ffad113eb0a5cb850d101c1fe180ffcf1e9caa2
Poul Fighter <pw88@10g.pl>
2023-09-16 15:24:15 +0200

    make boxes on screen optional,depending on the object_detection module installed

--- hauhau/decorators.py
+++ hauhau/decorators.py
@@ -1,10 +1,15 @@
 import numpy as np
 import datetime
 import cv2
-from object_detection.utils import visualization_utils as viz_utils
-
+try:
+    from object_detection.utils import visualization_utils as viz_utils
+except:
+    viz_utils = None
 
 def decorate_by_detections(frame: np.array, detections, labels_map, confidence_thresh) -> np.array:
+    if not viz_utils:
+        return frame
+    
     frame_with_detections = cv2.cvtColor(
         frame.copy(), cv2.COLOR_BGR2RGB)
     viz_utils.visualize_boxes_and_labels_on_image_array(
